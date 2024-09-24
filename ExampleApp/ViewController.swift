3abd6be578e638a8e287a04f59fe5e2007cc629e
Polina Sokolova <polly.sokolova@icloud.com>
2024-09-24 04:26:28 +0400

    Set up constraints

--- ExampleApp/ViewController.swift
+++ ExampleApp/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 	
 	private let helper = Helper()
+    
 	private let textLabel = UILabel()
 	private let imageView = UIImageView()
     private let imageContainerView = UIView()
@@ -23,8 +24,8 @@ class ViewController: UIViewController {
         setupImageView()
         view.addSubview(textLabel)
         view.addSubview(imageContainerView)
-		imageContainerView.addSubview(imageView)
 		setupView()
+        setupLayout()
 	}
 	
 	private func updateNumbers() {
@@ -36,33 +37,52 @@ class ViewController: UIViewController {
 		textLabel.text = "\(firstNumber ?? 0)"
 		textLabel.font = .systemFont(ofSize: 30, weight: .bold)
 		textLabel.textColor = .red
-		textLabel.frame = CGRect(x: 30, y: 30, width: 100, height: 50)
 	}
 	
-	private func setupImageView() {
-        imageView.frame = imageContainerView.bounds
-        imageView.image = UIImage(named: "raccoon")
-        imageView.layer.cornerRadius = 20
-        imageView.clipsToBounds = true
-	}
-    
     private func setupImageContainerView(){
-        imageContainerView.frame = CGRect(x: 30, y: 130, width: 100, height: 200)
         imageContainerView.layer.shadowColor = UIColor.black.cgColor
-        imageContainerView.layer.shadowOffset = CGSize(width: 15, height: 15)
-        imageContainerView.layer.shadowOpacity = 1
+        imageContainerView.layer.shadowOffset = CGSize(width: 5, height: 5)
+        imageContainerView.layer.shadowOpacity = 0.7
         imageContainerView.layer.shadowRadius = 10
     }
     
-	private func setupView() {
-		let gradient = CAGradientLayer()
-		gradient.frame = view.bounds
-		gradient.colors = [UIColor.green.cgColor, UIColor.blue.cgColor]
-		gradient.startPoint = CGPoint(x: 0, y: 0)
-		gradient.endPoint = CGPoint(x: 1, y: 1)
+    private func setupImageView() {
+        imageView.image = UIImage(named: "raccoon")
+        imageView.layer.cornerRadius = 20
+        imageView.clipsToBounds = true
+        
+        imageView.contentMode = .scaleAspectFit
+        
+        imageContainerView.addSubview(imageView)
+    }
+    
+    private func setupView() {
+        let gradient = CAGradientLayer()
+        gradient.frame = view.bounds
+        gradient.colors = [UIColor.green.cgColor, UIColor.blue.cgColor]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 1, y: 1)
 		
 		// Добавляем подслой к кнопке
 		view.layer.insertSublayer(gradient, at: 0)  // Градиент на фоне кнопки
 	}
+    
+    private func setupLayout() {
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        imageContainerView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            textLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            textLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageContainerView.topAnchor.constraint(equalTo: textLabel.bottomAnchor, constant: 150),
+            imageContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageContainerView.heightAnchor.constraint(equalToConstant: 200),
+            imageContainerView.widthAnchor.constraint(equalToConstant: 200),
+            imageView.topAnchor.constraint(equalTo: imageContainerView.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: imageContainerView.bottomAnchor),
+            imageView.leadingAnchor.constraint(equalTo: imageContainerView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: imageContainerView.trailingAnchor)
+        ])
+    }
 }
-
